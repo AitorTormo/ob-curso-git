5ac991b4e04f347f7546ad7d1854e891fdd1cedd
AitorTormo <96740149+AitorTormo@users.noreply.github.com>
2022-01-31 19:13:13 +0100

    Add files via upload

---
+++ Ejercicio2/main.py
@@ -0,0 +1,30 @@
+
+agenda = {}
+
+agenda['Jose'] = '678799444'
+agenda['Martí'] = '666382119'
+agenda['Carla'] = '632458711'
+agenda['Laia'] = '647854768'
+agenda['Aitor'] = '654832849'
+agenda['Marcos'] = '733281431'
+agenda['María'] = '669541187'
+
+print(agenda)
+
+agenda['Jose'] = '678799445'
+del agenda['Carla']
+
+print(agenda)
+
+print(agenda['Aitor'])
+
+nombre = input('username: ')
+telf = input('telefono: ')
+
+print(nombre, telf)
+
+
+
+
+
+
